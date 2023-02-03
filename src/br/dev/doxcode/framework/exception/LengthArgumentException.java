162df55be455e063befe1c4a9e169f33d83ae012
Marcio Alves <marcioalv@yahoo.com.br>
2023-02-03 09:35:44 -0300

    Create LengthArgumentException.java

---
+++ src/br/dev/doxcode/framework/exception/LengthArgumentException.java
@@ -0,0 +1,37 @@
+/*
+ * LengthArgumentException.java
+ *
+ * Copyright (C) 2023 DoxCode Tecnologia e Informação LTDA 
+ *
+ * Outras informações podem ser obtidas em:
+ *     https://www.doxcode.dev.br 
+ *
+ * Autor(es): Marcio Alves (marcioalv@yahoo.com.br)
+ *
+ * Criado em 02/Fevereiro/2023 por Marcio Alves (versão 1.0.0).
+ */
+
+package br.dev.doxcode.framework.exception;
+
+/**
+ * Exceção lançada para tamanhos inválidos de argumentos.
+ * 
+ * @author Marcio Alves
+ * @version 1.0.0 Criado em 02/Fevereiro/2023 por Marcio Alves.
+ */
+public class LengthArgumentException extends RuntimeException {
+	/**
+	 * Constante de serialização da classe.
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Construtor padrão que inicializa mensagem.
+	 * 
+	 * @author Marcio Alves
+	 * @version 1.0.0 Criado em 02/Fevereiro/2023 por Marcio Alves.
+	 */
+	public NegativeNumberArgumentException(String message) {
+		super(message);
+	}
+}
